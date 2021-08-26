7a5aff120b3d9e204fe07607e15c4b504ab36667
gaskillcw <gaskillcw@gmail.com>
2021-08-26 11:45:56 -0400

    setup order model

---
+++ Bartender/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bartender.Models
+{
+    public class Order
+    {
+        public int OrderID { get; set; }
+        public User Customer { get; set; }
+        public Drink Item { get; set; }
+    }
+}
