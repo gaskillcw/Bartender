5efdf45cea41e9c8a345ca3330d8637c00f22db5
gaskillcw <gaskillcw@gmail.com>
2021-08-26 10:45:32 -0400

    Added login action to home controller

--- Bartender/Controllers/HomeController.cs
+++ Bartender/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace Bartender.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
