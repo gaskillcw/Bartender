587a699d96a3129b1a087755cc2899db36f56599
gaskillcw <gaskillcw@gmail.com>
2021-08-26 11:04:56 -0400

    Added drink and user models and created lists eof each in the repository

--- Bartender/Repository/Repository.cs
+++ Bartender/Repository/Repository.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Bartender.Models;
 
 namespace Bartender.Repository
 {
     public class Repository
     {
+        public List<User> Users;
+        public List<Drink> Drinks;
     }
 }
